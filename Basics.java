f1e6a08996f51f2057d2b53acea47df1d8d2f524
Raj Singh <rajsingh.19.vns@gmail.com>
2024-12-19 18:12:11 +0530

    made chnages for confirmation

--- Basics.java
+++ Basics.java
@@ -1,5 +1,16 @@
 public class Basics {
     public static void main(String args[]) {
         System.out.println("Hello");
-    }
+
+        //  variable declare
+        int a = 8, b = 9;
+        // Calling the getSum method and printing the result
+        int Sum = getSum(a, b);
+        System.out.println("Sum of a: " + a + " and b: " + b + " is " + Sum);
+    };
+
+
+    public static int getSum(int a, int b) {
+        return a + b;
+    };
 }
